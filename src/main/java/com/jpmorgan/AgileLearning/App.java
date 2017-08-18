22497aab478db070715ed34c775cf596031b1576
bbalaganapathy <bbalaganapathy@gmail.com>
2017-08-18 13:44:43 +0530

    first git

---
+++ src/main/java/com/jpmorgan/AgileLearning/App.java
@@ -0,0 +1,13 @@
+package com.jpmorgan.AgileLearning;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/test/java/com/jpmorgan/AgileLearning/PalindromeTest.java
@@ -0,0 +1,32 @@
+package com.jpmorgan.AgileLearning;
+
+import junit.framework.TestCase;
+
+public class PalindromeTest extends TestCase {
+	
+	  
+	    public void testApp()
+	    {
+	        assertTrue(Palindrome.isPalindrome("MALAYALAM"));
+	    }
+	    
+	    public void testApp1()
+	    {
+	        assertFalse(Palindrome.isPalindrome("MAAMNN India "));
+	    }
+	    
+	    public void testException()
+	    {
+	    	boolean thrown=false;
+	    	try {
+	    		Palindrome.isPalindrome(null);
+	    	  } catch (Exception e) {
+	    	    thrown = true;
+	    	  }
+
+	    	  assertTrue(thrown);
+	       
+	    }
+	
+
+}
